6c78da6c232a3843b2b476fbe29df86cb112dc46
komarnetwork <komar.network@gmail.com>
2024-02-24 10:54:08 +0700

    component, props dan emit

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,47 +1,27 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <Student @response="showData" studentName="Andy" />
+  <Student @response="showData" studentName="Budi" />
+  <Student @response="showData" studentName="Ani" />
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <p>Student yang dipilih adalah : {{ selectedStudent }}</p>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
+<script>
+import Student from './components/Student.vue'
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
+export default {
+  components: {
+    Student
+  },
+  data() {
+    return {
+      selectedStudent: ''
+    }
+  },
+  methods: {
+    showData(whatever) {
+      this.selectedStudent = whatever
+    }
   }
 }
-</style>
+</script>
---
+++ vue-project/src/components/Student.vue
@@ -0,0 +1,17 @@
+<template>
+  <p>ini komponen student {{ studentName }} <button @click="showData">Show Detail</button></p>
+</template>
+
+<script>
+export default {
+  props: {
+    studentName: String
+  },
+  emits: ['response'],
+  methods: {
+    showData() {
+      this.$emit('response', this.studentName)
+    }
+  }
+}
+</script>
